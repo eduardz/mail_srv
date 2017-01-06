00131bd756f19809d92ed0a7894c0f6f7a9d6cc1
eduardz <ezz.edu@gmail.com>
2017-01-06 16:19:16 +0200

    vers: 3
    postfix_vda+Dovecot+Amavisd(spamaassassin+clam)+MariaDB(sql-quota)+Apache2.4+OpenDKIM+Apache-mod_security+policy-SPF+PostfixAdmin+Roundcube

--- mail_srv.sh
+++ mail_srv.sh
@@ -1,39 +1,41 @@
 #!/bin/bash
 echo "MAINTAINER Eduard Zaharia"
-echo "CentOS7 email server 2017 postfix+dovecot+mysql_quota+amavis(sa+clam)"
+echo "CentOS7_Postfix_VDA/Dovecot/Amavisd-New/PostfixAdmin+MySQL_Quota+SpamAssassin+ClamAV"
+sleep 3
 
+## <Variabile de schimbat> ###
 # Update program links
 WEB_LATEST_POSTFIXADM='https://netix.dl.sourceforge.net/project/postfixadmin/postfixadmin/postfixadmin-3.0/postfixadmin-3.0.tar.gz'
-
-## <Variables> ###
-USR_ID=5377
 #
-SQL_ROOT_PASS='change_root_SQL_pass'
+SRV_MAIL_IP=192.168.2.162
+USR_ID=30791
+#
+SQL_ROOT_PASS='change-SQL_change'
 POSTFIX_USER='vmail'
-POSTFIX_PASS='change-pass'
-POSTFIX_SQL_DB='mail-sql_somepass'
+POSTFIX_PASS='some_pass_to_change'
+POSTFIX_SQL_DB='go_mailDB'
 POSTFIX_MAIL_LOCATION=/var/vmail
 #
-ROUNDCUBE_USER='user-rd'
-ROUNDCUBE_PASS='chenge_pass_rd'
-ROUNDCUBE_DB='rd_db0_04'
+ROUNDCUBE_USER='ro0und'
+ROUNDCUBE_PASS='change_rouncube_pawss'
+ROUNDCUBE_DB='round_lDB'
 #
-HOSTNAME_WEB=mail.example.ro
-VH_ROUNCUBE='roundcube.example.ro'
-VH_POSTFIXADMIN='postfix.example.ro'
-SRV_ALIAS='webmail.example.ro'
-MAIL_ADMIN='dev@euroweb.ro'
-#
-## </Variables> ###
+HOSTNAME_WEB=mail.euroweb.ro
+VH_ROUNCUBE='roundcube.euroweb.ro'
+VH_POSTFIXADMIN='postfix.euroweb.ro'
+SRV_ALIAS='webmail.euroweb.ro'
+MAIL_ADMIN='dev@'
 
-# add email user-no-login #
+## </Variabile de schimbat> ###
+
+### install ##################################################
+# adduser
 groupadd -g $USR_ID $POSTFIX_USER
 useradd -g $USR_ID -u $USR_ID -d $POSTFIX_MAIL_LOCATION $POSTFIX_USER -s /sbin/nologin -c "virtual postfix user"
-### install ##################################################
 
-# 1_install_POSTFIX+VDA-patched # patch postfix-version+VDA_patch
+# echo "1_install_POSTFIX+VDA-patched"
 yum remove -y postfix
-yum localinstall -y /home/tmp/postfix*.rpm
+yum localinstall -y /home/ansible/postfix*.rpm
 
 # _install_preparation
 yum install -y epel-release 
@@ -43,7 +45,7 @@ yum install -y amavisd-new clamav-server-systemd clamav-update dovecot dovecot-m
 yum install -y mariadb mariadb-server mod_ssl mod_security ntp php php-imap spamassassin 
 # 9 packages to install
 yum install -y php-gd php-intl php-ldap php-mbstring php-mcrypt php-mysql php-xml roundcubemail opendkim
-# 1.4_install_PostfixAdmin"
+# install_PostfixAdmin"
 curl -s $WEB_LATEST_POSTFIXADM | tar zxvf - -C /var/www/html/
 mv /var/www/html/postfixadmin-*/ /var/www/html/postfixadmin
 chown apache:apache -R /var/www/html/postfixadmin
@@ -54,30 +56,29 @@ chown apache:apache -R /var/www/html/postfixadmin
 # set timezone
 sed -i "s/^;date.timezone =$/date.timezone = \"Europe\/Bucharest\"/" /etc/php.ini 
 
-# 2_enable @Boot_Services"
+# echo "2_enable @Boot_Services"
 systemctl enable postfix dovecot httpd mariadb amavisd clamd@amavisd spamassassin opendkim
 
-# 3.1_conf_RoundCube ##
+# conf_RoundCube ###
 cp /etc/roundcubemail/config.inc.php.sample /etc/roundcubemail/config.inc.php
 chown root:apache /etc/roundcubemail/config.inc.php
 echo "\$rcmail_config['force_https'] = true;" >> /etc/roundcubemail/config.inc.php
 echo "\$rcmail_config['preview_pane'] = true;" >> /etc/roundcubemail/config.inc.php
-# plain password compatible with crypt field #
+# plain password compatible with crypt field#
 echo "\$rcmail_config['imap_auth_type'] = 'LOGIN';" >> /etc/roundcubemail/config.inc.php
+# setup user/pass/db > roundcube
 sed -i "s|^\(\$config\['db_dsnw'\] =\).*$|\1 \'mysqli://${ROUNDCUBE_USER}:${ROUNDCUBE_PASS}@localhost/${ROUNDCUBE_DB}\';|" /etc/roundcubemail/config.inc.php
 
-# 3.2_conf_PostfixAdmin  ##
+# conf_PostfixAdmin-DB ###
 sed -i "s|^\(\$CONF\['configured'\] =\).*$|\1 \ true\;|" /var/www/html/postfixadmin/config.inc.php
 sed -i "s|^\(\$CONF\['database_user'\] =\).*$|\1 \'$POSTFIX_USER\';|" /var/www/html/postfixadmin/config.inc.php
 sed -i "s|^\(\$CONF\['database_password'\] =\).*$|\1 \'$POSTFIX_PASS\';|" /var/www/html/postfixadmin/config.inc.php
 sed -i "s|^\(\$CONF\['database_name'\] =\).*$|\1 \'$POSTFIX_SQL_DB\';|" /var/www/html/postfixadmin/config.inc.php
-# conf_PostfixAdmin-mailbox_quota / default user quota=1024MB
+# user mailbox set quota=1024MB
 sed -i "s|^\(\$CONF\['maxquota'\] =\).*$|\1 \'1024\';|" /var/www/html/postfixadmin/config.inc.php
 sed -i "s|^\(\$CONF\['quota'\] =\).*$|\1 \'YES\';|" /var/www/html/postfixadmin/config.inc.php
-# domain quota (default enabled)
-#sed -i "s|^\(\$CONF\['domain_quota'\] =\).*$|\1 \'NO\';|" /var/www/html/postfixadmin/config.inc.php
 
-# 4.1_create SSL-Certs ###
+# _create SSL-Certs ###
 mkdir -p /etc/httpd/ssl/
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/httpd/ssl/$HOSTNAME_WEB.key -out /etc/httpd/ssl/$HOSTNAME_WEB.crt <<EOF  
 RO
@@ -90,13 +91,16 @@ $MAIL_ADMIN
 EOF
 #
 
-# 4.1_conf_DKIM 
+# _config: DKIM
 mkdir -p /etc/opendkim/keys/$HOSTNAME_WEB
 /usr/sbin/opendkim-genkey -D /etc/opendkim/keys/$HOSTNAME_WEB/ -d $HOSTNAME_WEB -s dkim_selector
 chown -R opendkim:opendkim /etc/opendkim/keys/*
 echo "*@$HOSTNAME_WEB dkim_selector._domainkey.$HOSTNAME_WEB" >> /etc/opendkim/SigningTable
 echo "*dkim_selector._domainkey.$HOSTNAME_WEB $HOSTNAME_WEB:dkim_selector:/etc/opendkim/keys/$HOSTNAME_WEB/dkim_selector" >> /etc/opendkim/KeyTable
 echo "localhost" >> /etc/opendkim/TrustedHosts
+echo "$HOSTNAME_WEB" >> /etc/opendkim/TrustedHosts
+echo "SRV_MAIL_IP" >> /etc/opendkim/TrustedHosts
+#
 cat <<EOT > /etc/opendkim.conf
 ## opendkim.conf -- configuration file for OpenDKIM filter
 AutoRestart             Yes
@@ -118,7 +122,7 @@ UMask                   022
 UserID                  opendkim:opendkim
 EOT
 
-# 4.2_config_HTTPD #
+# _config_HTTPD=(vh_roundcube+vh_postfixadmin)
 rm -f /etc/httpd/conf.d/ssl.conf /etc/httpd/conf.d/welcome.conf /etc/httpd/conf.d/roundcubemail.conf
 
 echo "Listen 443" >> /etc/httpd/conf/httpd.conf
@@ -189,14 +193,14 @@ cat <<EOT >> /etc/httpd/conf.d/vh2_postfix-postfixadmin.conf
     <IfModule mod_authz_core.c>
         Require all granted
     </IfModule>
-#  setup.php = not secured
+# - setup.php = not secure
 #    <Files setup.php>
 #        Require all denied
 #    </Files>
 </Directory>
     Options -FollowSymLinks
     Header set X-XSS-Protection "1; mode=block"
-# Apache user/pass protection for postfixadmin
+# Apache user/pass protection
 #    <Location />
 #        Order allow,deny
 #        Allow from all
@@ -211,7 +215,7 @@ EOT
 #
 
 #################################################
-# 5_conf_PostFix/Dovecot - files
+# _PostFix/Dovecot MAPS
 cat <<EOT >> /etc/postfix/mysql_virtual_alias_maps.cf
 user = $POSTFIX_USER
 password = $POSTFIX_PASS
@@ -254,7 +258,7 @@ EOT
 cat <<EOT >> /etc/dovecot/dovecot-sql.conf.ext
 driver = mysql
 connect = host=localhost dbname=$POSTFIX_SQL_DB user=$POSTFIX_USER password=$POSTFIX_PASS
-default_pass_scheme = MD5-CRYPT
+default_pass_scheme = SHA512-CRYPT
 
 password_query = SELECT password FROM mailbox WHERE username = '%u' AND active = '1'
 
@@ -282,7 +286,7 @@ map {
   value_field = messages
 }
 EOT
-#
+# remove all config
 cat <<EOT > /etc/dovecot/conf.d/10-auth.conf
 !include auth-sql.conf.ext
 EOT
@@ -291,55 +295,72 @@ cat <<EOT > /etc/postfix/main.cf
 myhostname = $HOSTNAME_WEB
 mydomain = $HOSTNAME_WEB
 myorigin = \$myhostname
-
 inet_interfaces = all
 inet_protocols = ipv4
 mydestination = \$myhostname, localhost
 mynetworks = 127.0.0.0/8
-home_mailbox = Maildir/
 
+home_mailbox = Maildir/
 smtpd_banner = \$myhostname ESMTP Unknown
 disable_vrfy_command = yes
+broken_sasl_auth_clients = yes
 
-#smtpd_sender_restrictions =
-#        reject_rhsbl_sender zen.spamhaus.org
-#        reject_unknown_sender_domain
+# Certificates
+smtpd_tls_cert_file = /etc/httpd/ssl/$HOSTNAME_WEB.crt
+smtpd_tls_key_file = /etc/httpd/ssl/$HOSTNAME_WEB.key
 
-# Dovecot SASL
+# SMTP info http://www.postfix.org/TLS_README.html
+# may=Even though TLS encryption is always used, mail delivery continues even if the server certificate is untrusted/wrong name
+smtp_tls_security_level = may
+smtp_tls_loglevel = 1
+smtp_tls_protocols = !SSLv2, !SSLv3
+smtp_tls_mandatory_protocols = !SSLv2, !SSLv3
+
+smtpd_use_tls = yes
+smtpd_tls_loglevel = 1
+smtpd_tls_auth_only = yes
+# may=Postfix SMTP server announces STARTTLS support to remote SMTP clients, but does not require that clients use TLS encryption
+smtpd_tls_security_level = may
+smtpd_tls_protocols = !SSLv2, !SSLv3
+smtpd_tls_mandatory_protocols = !SSLv2, !SSLv3
+smtpd_tls_mandatory_ciphers = high
+smtpd_tls_mandatory_exclude_ciphers = aNULL, MD5
+smtpd_tls_session_cache_database = btree:/var/lib/postfix/smtpd_scache
+smtpd_tls_session_cache_timeout = 3600s
+
+smtpd_sasl_type = dovecot
+#smtpd_sasl_path = private/auth
+smtpd_sasl_path = /var/spool/postfix/private/auth
 smtpd_sasl_auth_enable = yes
 smtpd_sasl_local_domain = \$myhostname
 smtpd_sasl_security_options = noanonymous
 
-smtpd_sasl_type = dovecot
-smtpd_sasl_path = private/auth
-broken_sasl_auth_clients = yes
-smtpd_tls_security_level = encrypt
+smtpd_relay_restrictions =
+    permit_mynetworks
+    permit_tls_clientcerts
+    permit_sasl_authenticated
+    reject_unauth_destination
+
+smtpd_client_restrictions =
+#    check_client_access hash:/etc/postfix/access
+    reject_rbl_client zen.spamhaus.org
+    reject_rbl_client bl.spamcop.net
+    reject_rbl_client all.rbl.jp
+    reject_non_fqdn_sender
+    reject_unknown_sender_domain
+    reject_invalid_hostname
+
+smtpd_sender_restrictions =
+    reject_rhsbl_sender zen.spamhaus.org
+    reject_unknown_sender_domain
 
 smtpd_recipient_restrictions =
     permit_mynetworks
     permit_sasl_authenticated
     reject_unauth_destination
+    reject_non_fqdn_sender
+    reject_non_fqdn_recipient
     check_policy_service unix:private/policy-spf
-
-## CERTS
-smtpd_tls_cert_file = /etc/httpd/ssl/$HOSTNAME_WEB.crt
-smtpd_tls_key_file = /etc/httpd/ssl/$HOSTNAME_WEB.key
-
-smtp_tls_security_level = encrypt
-smtp_tls_loglevel = 1
-smtp_tls_protocols = !SSLv2, !SSLv3
-smtp_tls_mandatory_protocols = !SSLv2, !SSLv3
-smtpd_tls_protocols = !SSLv2, !SSLv3
-smtpd_tls_mandatory_protocols = !SSLv2, !SSLv3
-
-smtpd_tls_loglevel = 1
-smtpd_tls_session_cache_database = btree:/var/lib/postfix/smtpd_scache
-smtpd_tls_session_cache_timeout = 3600s
-
-# DKIM support for postfix
-smtpd_milters = inet:127.0.0.1:8891
-non_smtpd_milters = \$smtpd_milters
-milter_default_action = accept
 
 # Virtual Domain MySQL
 local_transport = local
@@ -354,14 +375,19 @@ virtual_uid_maps = static:$USR_ID
 virtual_gid_maps = static:$USR_ID
 virtual_minimum_uid = $USR_ID
 
-# Quota postfix_VDA Patch
+# DKIM support for postfix
+smtpd_milters = inet:127.0.0.1:8891
+non_smtpd_milters = \$smtpd_milters
+milter_default_action = accept
+
+# Virtual delivery Postfix_VDA patched
 virtual_maildir_extended = yes
 virtual_mailbox_limit_maps = mysql:/etc/postfix/mysql_virtual_mailbox_limit_maps.cf
 virtual_mailbox_limit_override = yes
 virtual_overquota_bounce = no
-virtual_trash_count = no
+virtual_trash_count = yes
 
-# Amavisd-New
+# Amavisd-new
 content_filter=smtp-amavis:[127.0.0.1]:10024
 receive_override_options = no_address_mappings
 EOT
@@ -369,9 +395,17 @@ EOT
 cat <<EOT > /etc/postfix/master.cf
 smtp      inet  n       -       n       -       -       smtpd
 submission inet n       -       n       -       -       smtpd
-  -o smtpd_tls_security_level=encrypt
-  -o smtpd_sasl_auth_enable=yes
-  -o smtpd_client_restrictions=permit_sasl_authenticated,reject
+# SASL authentication with dovecot
+    -o smtpd_tls_security_level=encrypt
+#    -o smtpd_sasl_auth_enable=yes
+    -o smtpd_sasl_type=dovecot
+    -o smtpd_sasl_path=private/auth
+    -o smtpd_sasl_security_options=noanonymous
+    -o smtpd_sasl_local_domain=$myhostname
+    -o smtpd_client_restrictions=permit_sasl_authenticated,reject
+    -o smtpd_recipient_restrictions=reject_non_fqdn_recipient,reject_unknown_recipient_domain,permit_sasl_authenticated,reject
+    -o smtpd_reject_unlisted_recipient=no
+    -o milter_macro_daemon_name=ORIGINATING
 
 smtps     inet  n       -       n       -       -       smtpd
   -o smtpd_tls_wrappermode=yes
@@ -393,6 +427,7 @@ smtp-amavis unix  -      -       n       -       2       smtp
      -o smtpd_helo_restrictions=
      -o smtpd_sender_restrictions=
      -o smtpd_recipient_restrictions=permit_mynetworks,reject
+     -o smtpd_data_restrictions=reject_unauth_pipelining
      -o mynetworks=127.0.0.0/8
      -o mynetworks_style=host
      -o strict_rfc821_envelopes=yes
@@ -401,11 +436,11 @@ smtp-amavis unix  -      -       n       -       2       smtp
      -o smtpd_hard_error_limit=1000
      -o receive_override_options=no_address_mappings
      -o receive_override_options=no_header_body_checks,no_unknown_recipient_checks
+     -o smtpd_milters=
 
 
 dovecot  unix    -       n       n       -       -       pipe
      flags=DRhu user=$POSTFIX_USER:$POSTFIX_USER argv=/usr/libexec/dovecot/deliver -f \${sender} -d \${recipient}
-#postfix-newer version#      flags=DRhu user=$POSTFIX_USER:$POSTFIX_USER argv=/usr/libexec/dovecot/deliver -f \${sender} -d \${user}@\${nexthop}
 
 # Prevent sender address forgery with Sender Policy Framework(SPF); postfix checks SPF record on all incoming email#
 policy-spf unix -       n       n       -       0       spawn
@@ -424,9 +459,8 @@ verify    unix  -       -       -       -       1       verify
 flush     unix  n       -       -       1000?   0       flush
 proxymap  unix  -       -       n       -       -       proxymap
 smtp      unix  -       -       n       -       -       smtp
-        -o smtp_bind_address=192.168.2.89 
-        #change_this_to email-IP
-#proxywrite unix -       -       n       -       1       proxymap
+        -o smtp_bind_address=$SRV_MAIL_IP
+proxywrite unix -       -       n       -       1       proxymap
 EOT
 
 # conf_Dovecot#################
@@ -589,7 +623,7 @@ plugin {
 }
 EOT
 
-# 6_conf_AMAVISD-New ##
+# _conf_AMAVISD-New
 sed -i "s|^\(\$mydomain\ =\).*$|\1 \'localhost\';|" /etc/amavisd/amavisd.conf
 cat <<EOT >> /etc/amavisd/amavisd.conf
  @lookup_sql_dsn = (
@@ -603,7 +637,7 @@ cat <<EOT >> /etc/amavisd/amavisd.conf
 \$recipient_delimiter = undef;
 EOT
 #
-## 7_config_CLAM@amavis ##
+## _config_CLAM@amavis
 sed -i -e 's/^Example/#Example/' /etc/freshclam.conf
 echo > /etc/sysconfig/freshclam
 
@@ -624,29 +658,28 @@ y
 EOF
 #
 
-# 8_conf SQL_DB: postfix-db / rouncube-db
+# postfix-db / rouncube-db
 mysql -uroot -p$SQL_ROOT_PASS -e "CREATE DATABASE $POSTFIX_SQL_DB CHARACTER SET utf8 COLLATE utf8_general_ci;" 
 mysql -uroot -p$SQL_ROOT_PASS -e "CREATE DATABASE $ROUNDCUBE_DB CHARACTER SET utf8 COLLATE utf8_general_ci;"
-#
+
 mysql -uroot -p$SQL_ROOT_PASS -e "CREATE USER $POSTFIX_USER@localhost IDENTIFIED BY '$POSTFIX_PASS';"
 mysql -uroot -p$SQL_ROOT_PASS -e "CREATE USER $ROUNDCUBE_USER@localhost IDENTIFIED BY '$ROUNDCUBE_PASS';"
-#
+
 mysql -uroot -p$SQL_ROOT_PASS -e "GRANT ALL PRIVILEGES ON $POSTFIX_SQL_DB.* TO '$POSTFIX_USER'@'localhost';"
 mysql -uroot -p$SQL_ROOT_PASS -e "GRANT ALL PRIVILEGES ON $ROUNDCUBE_DB.* TO '$ROUNDCUBE_USER'@'localhost';"
 mysql -uroot -p$SQL_ROOT_PASS -e "FLUSH PRIVILEGES;"
-#
+
 #initialize RoundCube-DB
 mysql -u root -p$SQL_ROOT_PASS $ROUNDCUBE_DB < /usr/share/roundcubemail/SQL/mysql.initial.sql
-#
+
+# cron #
+#spamassassin cron job location /etc/cron.d/sa-update
+
 echo "
-os=CentOS7
 installed Postfix_VDA+Dovecot+Amavisd(spamaassassin+clam)+MariaDB(MySQL)+Apache2.4+OpenDKIM+Apache-mod_security+policy-SPF+PostfixAdmin
-sleep 3
 ##
-to do
-- configure passwd postfixadmin in config + create admin user
+- configure pass postfixadmin in config + create admin
 - secure postfixadmin
-- setup iptables; cronjobs (sa-update; clam update; sql-dump)
-- conf DMARK
+- setup iptables, dkim add DNS text
 - "
 
